78d2e41b1c820ef6778bf03beb0049540dc85ca9
Luke <luke@lukekowald.com>
2012-11-05 19:07:09 +1030

    Updated files

--- server-components/save.php
+++ server-components/save.php
@@ -11,9 +11,14 @@ mysql_select_db($database, $con);
 $locationID = $_POST["lid"];
 $email = mysql_real_escape_string($_POST["email"]);
 $comment = mysql_real_escape_string($_POST["comment"]);
+//$locationID = 1;
+//$email = "luke@lukekowald.com";
+//$comment = "Testing 1 2 3";
 
 $sql = "INSERT INTO comments (location_id, email, comment) ";
 $sql .= "VALUES ($locationID, '$email', '$comment')";
+
+echo $sql;
 
 if (!mysql_query($sql, $con)) {
 	die('Error: ' . mysql_error());
